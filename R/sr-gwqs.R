116223b6bccdc4aca4a3862f0b25666e0f0c15d9
KunHuo <610112077@qq.com>
2023-03-14 13:07:08 +0800

    Update sr-gwqs.R

--- R/sr-gwqs.R
+++ R/sr-gwqs.R
@@ -1,3 +1,25 @@
+#' Fitting Weighted Quantile Sum regression models
+#'
+#' @param data a data frame.
+#' @param outcome outcome variable name.
+#' @param exposure exposure variable names.
+#' @param covariates covariate variable names.
+#' @param q An integer to specify how mixture variables will be ranked, e.g. in
+#' quartiles (q = 4), deciles (q = 10), or percentiles (q = 100). If q = NULL
+#' then the values of the mixture variables are taken (these must be standardized).
+#' @param b number of bootstrap samples used in parameter estimation.
+#' @param validation percentage of the dataset to be used to validate the model.
+#' If validation = 0 then the test dataset is used as validation dataset too.
+#' @param family a character value that allows to decide for the glm: gaussian
+#' for linear regression, binomial for logistic regression "multinomial" for
+#' multinomial regression, poisson for Poisson regression, quasipoisson for
+#' quasi-Poisson regression, "negbin" for negative binomial regression.
+#' @param seed an integer value to fix the seed, if it is equal to NULL no seed
+#' is chosen.
+#' @param ... additional arguments to be passed to the [gWQS::gwqs()] function.
+#'
+#' @return a list.
+#' @export
 sr_gwqs <- function(data,
                     outcome = NULL,
                     exposure = NULL,
@@ -71,5 +93,6 @@ sr_gwqs <- function(data,
        outcome = outcome,
        exposure = exposure,
        covariates = covariates,
+       family = family,
        b = b)
 }
